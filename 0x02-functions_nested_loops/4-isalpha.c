6d34a1942dfa195a04d0c87606c923f0729b4f3c
Tilly8 <nmatildav@gmail.com>
2023-04-26 15:38:25 -0700

    Sign

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,9 +3,9 @@
 /**
  * _isalpha - checks for alphabetic character
  * @c: the character to be checked
- * Return: 1 if c is a letter, lowercase or uppercase, 0 if otherwise
+ * Return: 1 if c is a letter, lowercase or uppercase
+ * 0 if otherwise
  */
-
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
